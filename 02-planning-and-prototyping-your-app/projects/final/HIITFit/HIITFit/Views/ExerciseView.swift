8574321c3e818b4296f941f92d5593c01126932a
mataharimau <audrey.tam@gmail.com>
2020-09-17 08:07:15 +1000

    Added appiconset; moved arrays inside struct ExerciseView

--- 02-planning-and-prototyping-your-app/projects/final/HIITFit/HIITFit/Views/ExerciseView.swift
+++ 02-planning-and-prototyping-your-app/projects/final/HIITFit/HIITFit/Views/ExerciseView.swift
@@ -33,10 +33,10 @@
 import SwiftUI
 import AVKit
 
-let videoNames = ["squat", "step-up", "burpee", "sun-salute"]
-let exerciseNames = ["Squat", "Step Up", "Burpee", "Sun Salute"]
-
 struct ExerciseView: View {
+  let videoNames = ["squat", "step-up", "burpee", "sun-salute"]
+  let exerciseNames = ["Squat", "Step Up", "Burpee", "Sun Salute"]
+
   let index: Int
   let interval: TimeInterval = 30
 
