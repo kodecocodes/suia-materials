1346c7034ac5f0c43ec95fe3652343fd0a31ad76
Caroline <caroline@archway.com.au>
2020-12-04 15:32:10 +1000

    First draft starter project

--- 15-saving-files/projects/starter/Cards/Cards/Extensions/ColorExtensions.swift
+++ 15-saving-files/projects/starter/Cards/Cards/Extensions/ColorExtensions.swift
@@ -41,3 +41,29 @@ extension Color {
     randomColors.randomElement() ?? .black
   }
 }
+
+extension Color {
+  func colorComponents() -> [CGFloat] {
+    let uiColor = UIColor(self)
+    var red: CGFloat = 0
+    var green: CGFloat = 0
+    var blue: CGFloat = 0
+    var alpha: CGFloat = 0
+
+    uiColor.getRed(
+      &red,
+      green: &green,
+      blue: &blue,
+      alpha: &alpha)
+    return [red, green, blue, alpha]
+  }
+
+  static func color(components color: [CGFloat]) -> Color {
+    let uiColor = UIColor(
+      red: color[0],
+      green: color[1],
+      blue: color[2],
+      alpha: color[3])
+    return Color(uiColor)
+  }
+}
