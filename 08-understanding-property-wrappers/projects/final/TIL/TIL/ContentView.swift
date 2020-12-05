b0392a712a0d1b5929fd0122a6247d050f67cbb5
mataharimau <audrey.tam@gmail.com>
2020-12-05 15:51:54 +1100

    early draft

---
+++ 08-understanding-property-wrappers/projects/final/TIL/TIL/ContentView.swift
@@ -0,0 +1,77 @@
+/// Copyright (c) 2020 Razeware LLC
+///
+/// Permission is hereby granted, free of charge, to any person obtaining a copy
+/// of this software and associated documentation files (the "Software"), to deal
+/// in the Software without restriction, including without limitation the rights
+/// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+/// copies of the Software, and to permit persons to whom the Software is
+/// furnished to do so, subject to the following conditions:
+///
+/// The above copyright notice and this permission notice shall be included in
+/// all copies or substantial portions of the Software.
+///
+/// Notwithstanding the foregoing, you may not use, copy, modify, merge, publish,
+/// distribute, sublicense, create a derivative work, and/or sell copies of the
+/// Software in any work that is designed, intended, or marketed for pedagogical or
+/// instructional purposes related to programming, coding, application development,
+/// or information technology.  Permission for such use, copying, modification,
+/// merger, publication, distribution, sublicensing, creation of derivative works,
+/// or sale is expressly withheld.
+///
+/// This project and source code may use libraries or frameworks that are
+/// released under various Open-Source licenses. Use of those libraries and
+/// frameworks are governed by their own individual licenses.
+///
+/// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+/// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+/// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+/// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+/// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+/// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+/// THE SOFTWARE.
+
+import SwiftUI
+
+class ThingStore: ObservableObject {
+  @Published var things: [String] = []
+}
+
+struct ContentView: View {
+  @State private var showAddThing = false
+  @StateObject private var myThings = ThingStore()
+
+  var body: some View {
+    NavigationView {
+      VStack(spacing: 20) {
+        if myThings.things.isEmpty {
+          Text("Add acronyms you learn")
+            .foregroundColor(.gray)
+        }
+        ForEach(myThings.things, id: \.self) { thing in
+          Text(thing)
+        }
+        Spacer()
+      }
+      .sheet(isPresented: $showAddThing) {
+        AddThingView(someThings: myThings)
+      }
+      .navigationTitle("TIL")
+      .toolbar {
+        ToolbarItem {
+          // swiftlint:disable:next multiple_closures_with_trailing_closure
+          Button(action: { showAddThing.toggle() }) {
+            Image(systemName: "plus.circle")
+              .font(.title)
+          }
+        }
+      }
+    }
+  }
+}
+
+struct ContentView_Previews: PreviewProvider {
+  static var previews: some View {
+    ContentView()
+      .environment(\.textCase, .uppercase)
+  }
+}
