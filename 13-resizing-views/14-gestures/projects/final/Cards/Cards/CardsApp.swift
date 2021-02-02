d25a6cb800ba6901f5f684d21d044cd38985111b
Caroline Begbie <c.b100@aol.com>
2021-02-02 15:25:38 +1000

    Added StateObject

--- 13-resizing-views/14-gestures/projects/final/Cards/Cards/CardsApp.swift
+++ 13-resizing-views/14-gestures/projects/final/Cards/Cards/CardsApp.swift
@@ -34,10 +34,12 @@ import SwiftUI
 
 @main
 struct CardsApp: App {
+  @StateObject var viewState = ViewState()
+
   var body: some Scene {
     WindowGroup {
       CardsView()
-        .environmentObject(ViewState())
+        .environmentObject(viewState)
     }
   }
 }
