32dd08f8a864e76944bc159d81ecc5d796a973a1
Caroline Begbie <c.b100@aol.com>
2021-02-02 21:05:52 +1000

    Refactored chapter

--- 14-paths-and-custom-shapes/projects/challenge/Cards/Cards/Model/Shapes.swift
+++ 14-paths-and-custom-shapes/projects/challenge/Cards/Cards/Model/Shapes.swift
@@ -1,15 +1,15 @@
-///// Copyright (c) 2021 Razeware LLC
-///
+/// Copyright (c) 2021 Razeware LLC
+/// 
 /// Permission is hereby granted, free of charge, to any person obtaining a copy
 /// of this software and associated documentation files (the "Software"), to deal
 /// in the Software without restriction, including without limitation the rights
 /// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
 /// copies of the Software, and to permit persons to whom the Software is
 /// furnished to do so, subject to the following conditions:
-///
+/// 
 /// The above copyright notice and this permission notice shall be included in
 /// all copies or substantial portions of the Software.
-///
+/// 
 /// Notwithstanding the foregoing, you may not use, copy, modify, merge, publish,
 /// distribute, sublicense, create a derivative work, and/or sell copies of the
 /// Software in any work that is designed, intended, or marketed for pedagogical or
@@ -17,7 +17,7 @@
 /// or information technology.  Permission for such use, copying, modification,
 /// merger, publication, distribution, sublicensing, creation of derivative works,
 /// or sale is expressly withheld.
-///
+/// 
 /// This project and source code may use libraries or frameworks that are
 /// released under various Open-Source licenses. Use of those libraries and
 /// frameworks are governed by their own individual licenses.
@@ -56,13 +56,6 @@ struct Shapes: View {
       .padding()
       .aspectRatio(1, contentMode: .fit)
       .background(Color.yellow)
-  }
-}
-
-struct Shapes_Previews: PreviewProvider {
-  static var previews: some View {
-    Shapes()
-      .previewLayout(.sizeThatFits)
   }
 }
 
@@ -231,5 +224,13 @@ struct Polygon: Shape {
     }
     path.closeSubpath()
     return path
+  }
+}
+
+
+struct Shapes_Previews: PreviewProvider {
+  static var previews: some View {
+    Shapes()
+      .previewLayout(.sizeThatFits)
   }
 }
