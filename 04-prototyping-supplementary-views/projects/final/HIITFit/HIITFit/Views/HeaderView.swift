46ef0cae1e0ea5d4ffc8903a24ddc4e521f7060e
Libranner <libranner@gmail.com>
2022-10-16 18:45:29 +0200

    Fix indentation

--- 04-prototyping-supplementary-views/projects/final/HIITFit/HIITFit/Views/HeaderView.swift
+++ 04-prototyping-supplementary-views/projects/final/HIITFit/HIITFit/Views/HeaderView.swift
@@ -52,8 +52,8 @@ struct HeaderView: View {
 }
 
 struct HeaderView_Previews: PreviewProvider {
-    static var previews: some View {
-      HeaderView(titleText: "Squat")
-        .previewLayout(.sizeThatFits)
-    }
+  static var previews: some View {
+    HeaderView(titleText: "Squat")
+      .previewLayout(.sizeThatFits)
+  }
 }
